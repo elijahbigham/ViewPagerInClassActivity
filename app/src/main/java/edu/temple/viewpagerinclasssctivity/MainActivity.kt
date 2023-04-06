62f1008caaebced817f6d83470d085a5d6963e3b
Elijah Bigham <elijahbigham@gmail.com>
2023-04-06 15:20:42 -0400

    Added an add button that creates a fragment at the end, and then scrolls to the end.

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -11,12 +12,27 @@ class MainActivity : AppCompatActivity() {
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
     }
+    private val newButton: Button by lazy {
+        findViewById(R.id.addButton)
+    }
+    private var numberOfPages = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        newButton.setOnClickListener{
+            numberOfPages++
+            viewPager.adapter?.notifyItemInserted(numberOfPages-1)
+            viewPager.setCurrentItem(numberOfPages-1, true)
+
+
+            //supportFragmentManager.findFragmentByTag("id")
+        }
+
         viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numberOfPages
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
 
